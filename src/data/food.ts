df7f90f123cc9661a7260d928cddf4b813f5f027
Joao Victor <joao15victor08@gmail.com>
2023-02-04 12:57:25 -0300

    Atualiza tipo de peso e fome

--- src/data/food.ts
+++ src/data/food.ts
@@ -1,182 +1,182 @@
 export const food = [
     {
         "name": "Apple",
-        "hunger": " 30",
-        "weight": "10",
+        "hunger": 30,
+        "weight": 10,
         "image": "https://static.wikia.nocookie.net/play-rust/images/d/dc/Apple_icon.png/revision/latest?cb=20151106061034"
     },
     {
         "name": "Rotten Apple",
-        "hunger": " 15",
-        "weight": "1",
+        "hunger": 15,
+        "weight": 1,
         "image": "https://static.wikia.nocookie.net/play-rust/images/b/bf/Rotten_Apple_icon.png/revision/latest?cb=20151106060901"
     },
     {
         "name": "Bear Meat",
-        "hunger": " 100",
-        "weight": "20",
+        "hunger": 100,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/c/c8/Bear_Meat_icon.png/revision/latest?cb=20151106054440"
     },
     {
         "name": "Bear Meat",
-        "hunger": " 100",
-        "weight": "20",
+        "hunger": 100,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/c/c8/Bear_Meat_icon.png/revision/latest?cb=20151106054440"
     },
     {
         "name": "Burnt Bear Meat",
-        "hunger": " 25",
-        "weight": "20",
+        "hunger": 25,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/c/c2/Burnt_Bear_Meat_icon.png/revision/latest?cb=20160109015134"
     },
     {
         "name": "Black Raspberries",
-        "hunger": " 40",
-        "weight": "20",
+        "hunger": 40,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/6/6f/Black_Raspberries_icon.png/revision/latest?cb=20151119214047"
     },
     {
         "name": "Blueberries",
-        "hunger": " 30",
-        "weight": "20",
+        "hunger": 30,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/f/f8/Blueberries_icon.png/revision/latest?cb=20151106061231"
     },
     {
         "name": "Can of Beans",
-        "hunger": " 100",
-        "weight": "10",
+        "hunger": 100,
+        "weight": 10,
         "image": "https://static.wikia.nocookie.net/play-rust/images/e/e5/Can_of_Beans_icon.png/revision/latest?cb=20151106060935"
     },
     {
         "name": "Cactus Flesh",
-        "hunger": " 5",
-        "weight": "10",
+        "hunger": 5,
+        "weight": 10,
         "image": "https://static.wikia.nocookie.net/play-rust/images/f/fe/Cactus_Flesh_icon.png/revision/latest/scale-to-width-down/350?cb=20160901064354"
     },
     {
         "name": "Candy Cane",
-        "hunger": " 0",
-        "weight": "1",
+        "hunger": 0,
+        "weight": 1,
         "image": "https://static.wikia.nocookie.net/play-rust/images/2/2c/Candy_Cane_icon.png/revision/latest/scale-to-width-down/350?cb=20151217224745"
     },
     {
         "name": "Raw Chicken Breast",
-        "hunger": " 40",
-        "weight": "20",
+        "hunger": 40,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/8/81/Raw_Chicken_Breast_icon.png/revision/latest?cb=20151108000755"
     },
     {
         "name": "Chicken Breast",
-        "hunger": " 40",
-        "weight": "20",
+        "hunger": 40,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/8/81/Raw_Chicken_Breast_icon.png/revision/latest?cb=20151108000755"
     },
     {
         "name": "Burned Chicken",
-        "hunger": " 15",
-        "weight": "20",
+        "hunger": 15,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/b/bb/Burned_Chicken_icon.png/revision/latest?cb=20151108000750"
     },
     {
         "name": "Chocolate Bar",
-        "hunger": " 100",
-        "weight": "10",
+        "hunger": 100,
+        "weight": 10,
         "image": "https://static.wikia.nocookie.net/play-rust/images/4/45/Chocolate_Bar_icon.png/revision/latest?cb=20151106061914"
     },
     {
         "name": "Raw Deer Meat",
-        "hunger": " 40",
-        "weight": "20",
+        "hunger": 40,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/5/5e/Raw_Deer_Meat_icon.png/revision/latest/scale-to-width-down/350?cb=20170721153628"
     },
     {
         "name": "Cooked Deer Meat",
-        "hunger": " 40",
-        "weight": "20",
+        "hunger": 40,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/7/78/Cooked_Deer_Meat_icon.png/revision/latest/scale-to-width-down/350?cb=20170721153553"
     },
     {
         "name": "Burned Deer Meat",
-        "hunger": " 0",
-        "weight": "20",
+        "hunger": 0,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/9/97/Burned_Deer_Meat_icon.png/revision/latest/scale-to-width-down/350?cb=20170721153534"
     },
     {
         "name": "Granola Bar",
-        "hunger": " 60",
-        "weight": "10",
+        "hunger": 60,
+        "weight": 10,
         "image": "https://static.wikia.nocookie.net/play-rust/images/6/6c/Granola_Bar_icon.png/revision/latest?cb=20151106060916"
     },
     {
         "name": "Raw Human Meat",
-        "hunger": " 6",
-        "weight": "20",
+        "hunger": 6,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/2/26/Raw_Human_Meat_icon.png/revision/latest?cb=20151106061310"
     },
     {
         "name": "Cooked Human Meat",
-        "hunger": " 30",
-        "weight": "20",
+        "hunger": 30,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/d/d2/Cooked_Human_Meat_icon.png/revision/latest/scale-to-width-down/350?cb=20150405113229"
     },
     {
         "name": "Burned Human Meat",
-        "hunger": " 6",
-        "weight": "20",
+        "hunger": 6,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/f/f0/Burned_Human_Meat_icon.png/revision/latest/scale-to-width-down/350?cb=20150405140835"
     },
     {
         "name": "Mushroom",
-        "hunger": " 15",
-        "weight": "10",
+        "hunger": 15,
+        "weight": 10,
         "image": "https://static.wikia.nocookie.net/play-rust/images/a/a8/Mushroom_icon.png/revision/latest?cb=20151106060836"
     },
     {
         "name": "Pork",
-        "hunger": " 60",
-        "weight": "20",
+        "hunger": 60,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/7/78/Pork_icon.png/revision/latest/scale-to-width-down/350?cb=20160211201227"
     },
     {
         "name": "Pork",
-        "hunger": " 60",
-        "weight": "20",
+        "hunger": 60,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/7/78/Pork_icon.png/revision/latest/scale-to-width-down/350?cb=20160211201227"
     },
     {
         "name": "Burned Pork",
-        "hunger": " 10",
-        "weight": "20",
+        "hunger": 10,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/6/61/Burned_Pork_icon.png/revision/latest/scale-to-width-down/350?cb=20160211201143"
     },
     {
         "name": "Pumpkin",
-        "hunger": " 100",
-        "weight": "20",
+        "hunger": 100,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/4/4c/Pumpkin_icon.png/revision/latest?cb=20151106061814"
     },
     {
         "name": "Tuna Can",
-        "hunger": " 50",
-        "weight": "10",
+        "hunger": 50,
+        "weight": 10,
         "image": "https://static.wikia.nocookie.net/play-rust/images/2/2d/Can_of_Tuna_icon.png/revision/latest?cb=20151106061943"
     },
     {
         "name": "Raw Wolf Meat",
-        "hunger": " 20",
-        "weight": "20",
+        "hunger": 20,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/5/5c/Raw_Wolf_Meat_icon.png/revision/latest?cb=20160131235324"
     },
     {
         "name": "Wolf Meat",
-        "hunger": " 20",
-        "weight": "20",
+        "hunger": 20,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/5/5c/Raw_Wolf_Meat_icon.png/revision/latest?cb=20160131235324"
     },
     {
         "name": "Burned Wolf Meat",
-        "hunger": " 15",
-        "weight": "20",
+        "hunger": 15,
+        "weight": 20,
         "image": "https://static.wikia.nocookie.net/play-rust/images/b/b6/Burned_Wolf_Meat_icon.png/revision/latest?cb=20160129014232"
     }
 ]
